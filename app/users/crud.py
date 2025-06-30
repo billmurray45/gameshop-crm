fa98e40a065166aba27a14b50dc69808cb5bf7c9
billmurray45 <billfookingmurray@.com>
2025-06-30 17:04:56 +0500

    modified crud

--- app/users/crud.py
+++ app/users/crud.py
@@ -1,18 +1,34 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.users.models import User
-from app.users.schemas import UserCreate, UserUpdate
-from app.auth.security import get_password_hash
 
 
-async def create_user(session: AsyncSession, user_data: UserCreate) -> User:
-    user = User(
-        email=user_data.email,
-        username=user_data.username,
-        full_name=user_data.full_name,
-        hashed_password=get_password_hash(user_data.password)
-    )
+async def insert_user(session: AsyncSession, user: User) -> User:
     session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
+
+
+async def get_user_by_id(session: AsyncSession, user_id: int) -> User:
+    result = await session.execute(select(User).where(User.id == user_id))
+    user = result.scalars().first()
+    return user
+
+
+async def get_user_by_email(session: AsyncSession, email: str) -> User:
+    result = await session.execute(select(User).where(User.email == email))
+    user = result.scalars().first()
+    return user
+
+
+async def get_user_by_username(session: AsyncSession, username: str) -> User:
+    result = await session.execute(select(User).where(User.username == username))
+    user = result.scalars().first()
+    return user
+
+
+async def update_user(session: AsyncSession, user: User) -> User:
     await session.commit()
     await session.refresh(user)
     return user
